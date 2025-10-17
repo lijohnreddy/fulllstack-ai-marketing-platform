2721c1ffacd7c7dc46e46c99292d991410f4334a
joseph <ryakalajoseph736@gmail.com>
2025-10-18 02:11:02 +0530

    Add nextjs as regular directory

---
+++ nextjs/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "../components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-red-500 text-white">
+        Hello Welcome to your new beginning
+      </div>
+      <Button variant={"ghost"}>Click me</Button>
+    </div>
+  );
+}
