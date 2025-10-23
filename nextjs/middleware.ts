3cbec3ad2a22adc2e4c1d32b8220b544d300d8d3
joseph <ryakalajoseph736@gmail.com>
2025-10-24 01:08:27 +0530

    auth() moiddleware

--- nextjs/middleware.ts
+++ nextjs/middleware.ts
@@ -5,24 +5,21 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in",
   "/sign-up",
   "/pricing",
+  "/projects", // Add this line to make /projects public temporarily
 ]);
 
-// This example protects all routes including api/trpc routes
-// Please edit this to allow other routes to be public as needed.
-// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about Clerk's Next.js middleware
-export default clerkMiddleware((auth, request) => {
+export default clerkMiddleware(async (auth, request) => {
   if (!isPublicRoute(request)) {
-    return auth().then((userAuth) => {
-      if (!userAuth.isAuthenticated) {
-        return Response.redirect("/sign-in");
-      }
-    });
+    // Use await instead of .then()
+    const session = await auth();
+    if (!session.userId) {
+      return Response.redirect(new URL("/sign-in", request.url));
+    }
   }
 });
 
 export const config = {
   matcher: [
-    // Exclude files with a contents-hash, `_next` routes, and Clerk's internal routes
-    "/((?!.+.[w]+$|_next/static|_next/image|favicon.ico|_clerk).*)",
+    "/((?!.+\\.[\\w]+$|_next/static|_next/image|favicon.ico|_clerk).*)",
   ],
 };
